e5bfd30c45f315f4827efc792f98834403326ac0
Minhajulislam0 <mdmishu98730@gmail.com>
2023-09-07 00:03:24 +0600

    Responsive Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,6 +1,42 @@
 import "./Header.scss";
+import { TbSearch } from "react-icons/tb";
+import { AiOutlineHeart } from "react-icons/ai";
+import { CgShoppingCart } from "react-icons/cg";
+import { useEffect, useState } from "react";
 const Header = () => {
-    return <div>Header</div>;
+  const [scrolled, setScrolled] = useState(false);
+  const handleScroll = () => {
+    const offset = window.scrollY;
+    if (offset > 200) {
+      setScrolled(true);
+    } else {
+      setScrolled(false);
+    }
+  };
+  useEffect(() => {
+    window.addEventListener("scroll", handleScroll);
+  }, []);
+
+  return (
+    <header className={`main-header ${scrolled ? "sticky-header" : ""}`}>
+      <div className="header-content">
+        <ul className="left">
+          <li>Home</li>
+          <li>About</li>
+          <li>Category</li>
+        </ul>
+        <div className="center">Gadget Guys</div>
+        <div className="right">
+          <TbSearch />
+          <AiOutlineHeart />
+          <span className="cart-icon">
+            <CgShoppingCart />
+            <span>2</span>
+          </span>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
