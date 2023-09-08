0909035a2e9c9bfe94cfbf5f9b1f913d5920acb4
Minhajulislam0 <mdmishu98730@gmail.com>
2023-09-09 02:23:16 +0600

    Responsive Newsletter Created

--- src/components/Footer/Newsletter/Newsletter.jsx
+++ src/components/Footer/Newsletter/Newsletter.jsx
@@ -1,6 +1,41 @@
 import "./Newsletter.scss";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+} from "react-icons/fa";
+
 const Newsletter = () => {
-    return <div>Newsletter</div>;
+  return (
+    <div className="newsletter-section">
+      <div className="newsletter-content">
+        <span className="small-text">Newsletter</span>
+        <span className="big-text">Sign up for latest update and offers.</span>
+        <div className="form">
+          <input type="email" placeholder="Email Address" />
+          <button>Subscribe</button>
+        </div>
+        <div className="text">
+          Will be used accordance with our <span>Privacy Policy</span>.
+        </div>
+        <div className="social-icons">
+          <div className="icon">
+            <FaFacebookF />
+          </div>
+          <div className="icon">
+            <FaInstagram />
+          </div>
+          <div className="icon">
+            <FaLinkedinIn />
+          </div>
+          <div className="icon">
+            <FaTwitter />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Newsletter;
