5d375ce0aa8256ce1d8f36f5e5a9368bb5dba74e
Minhajulislam0 <mdmishu98730@gmail.com>
2023-09-11 22:48:12 +0600

    Responsive Caetegory page

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,4 +1,4 @@
-import Category from "../Category/Category";
+import Category from "./Category/Category";
 import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 import "./Home.scss";
