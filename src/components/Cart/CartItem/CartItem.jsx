13189956a702235b22f5e1160e4bc1265ac9f642
Minhajulislam0 <mdmishu98730@gmail.com>
2023-09-13 03:50:44 +0600

    Cart product

--- src/components/Cart/CartItem/CartItem.jsx
+++ src/components/Cart/CartItem/CartItem.jsx
@@ -1,6 +1,30 @@
 import "./CartItem.scss";
+import { MdClose } from "react-icons/md";
+import prod from "../../../assets/products/watch-prod-3.webp";
 const CartItem = () => {
-    return <div>Cart item</div>;
+  return (
+    <div className="cart-products">
+      <div className="cart-product">
+        <div className="img-container">
+          <img src={prod} alt="" />
+        </div>
+        <div className="prod-details">
+          <span className="name">Smart Watch</span>
+          <MdClose className="close-button" />
+          <div className="quantity-buttons">
+            <span>-</span>
+            <span>5</span>
+            <span>+</span>
+          </div>
+          <div className="text">
+            <span>3</span>
+            <span>x</span>
+            <span className="highlight">$232</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default CartItem;
