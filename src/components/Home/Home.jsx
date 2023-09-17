12890350336b3bd66c15b9b385d5f6fb43c51f7f
Minhajulislam0 <mdmishu98730@gmail.com>
2023-09-17 20:31:48 +0600

    dynamically added data from api

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -2,18 +2,33 @@ import Category from "./Category/Category";
 import Products from "../Products/Products";
 import Banner from "./Banner/Banner";
 import "./Home.scss";
-import { useEffect } from "react";
+import { useEffect, useContext } from "react";
 import { fetchDataFromApi } from "../../utils/api";
+import { Context } from "../../utils/context";
 
 const Home = () => {
+  const { categories, setCategories, products, setProducts } =
+    useContext(Context);
+
   useEffect(() => {
     getCategories();
   }, []);
 
+  useEffect(() => {
+    getProducts();
+  }, []);
+
   const getCategories = () => {
-    fetchDataFromApi("/api/categories?populate=*").then((res) =>
-      console.log(res)
-    );
+    fetchDataFromApi("/api/categories?populate=*").then((res) => {
+      console.log(res);
+      setCategories(res);
+    });
+  };
+  const getProducts = () => {
+    fetchDataFromApi("/api/products?populate=*").then((res) => {
+      console.log(res);
+      setProducts(res);
+    });
   };
 
   return (
@@ -21,8 +36,8 @@ const Home = () => {
       <Banner />
       <div className="main-content">
         <div className="layout">
-          <Category />
-          <Products headingText="Popular Products" />
+          <Category categories={categories} />
+          <Products products={products} headingText="Popular Products" />
         </div>
       </div>
     </div>
